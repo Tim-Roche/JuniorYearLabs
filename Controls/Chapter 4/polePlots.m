90bc2ed5dfb9fad7fc81ed59a85e5d4d6875f254
Tim Roche Laptop <kc2vsa@gmail.com>
2020-02-12 12:34:13 -0500

    Fixed annoying error in Controls

--- Controls/Chapter 4/polePlots.m
+++ Controls/Chapter 4/polePlots.m
@@ -2,8 +2,8 @@ clear all;
 close all;
 
 % Insert your code here
-a_vect = [4 8 2 4 4 8 32];
-b_vect = [25 37 22 88 340 100 1600];
+a_vect = [4 8 2 4 4 8 16];
+b_vect = [25 37 22 88 340 100 400];
 
 s = tf("s");
 
@@ -17,10 +17,10 @@ for n=1:length(a_vect)
     %Plotting Step Response
     a = a_vect(n);
     b = b_vect(n);
-    simOut = sim('question2.slx','ReturnWorkspaceOutputs','on');
-    output = stepinfo(simOut.question1.Data);
-    stepinfo( simOut.question1.Data, simOut.question1.Time)
-    plot(simOut.question1.Time, simOut.question1.Data)
+    simOut = sim('question.slx','ReturnWorkspaceOutputs','on');
+    output = stepinfo(simOut.question.Data);
+    stepinfo( simOut.question.Data, simOut.question.Time)
+    plot(simOut.question.Time, simOut.question.Data)
 end
 hold off
 
