17537bf4f18c73e5542c5fb7690885e082a5d777
Tim Roche Laptop <kc2vsa@gmail.com>
2020-02-14 17:15:56 -0500

    Fixed typo

--- Modules/Homework_3/question5.m
+++ Modules/Homework_3/question5.m
@@ -5,7 +5,6 @@ prob100th = geopdf(100, probs);
 disp("Prob on 1st: "+prob1st*100);
 disp("Prob on 100th: "+prob100th*100);
 
-%See: https://math.stackexchange.com/questions/428496/probability-of-of-an-event-happening-at-least-once-in-a-sequence-of-independent
 n = 5000;
-onePlode = geocdf(5000,(1/5000));
-disp("Prob of 1 Plode: "+onePlode*100);
+oneExplosion = geocdf(5000,(1/5000));
+disp("Prob of 1 Explosion: "+oneExplosion*100);
